3153e28d409d8d33f26eaf08ea3f8b6d2aa457a4
Peyal Hasan <peyalhasan143@gmail.com>
2025-10-20 22:42:47 +0600

    Async Await

---
+++ comments.js
@@ -0,0 +1,7 @@
+ 
+const loadComments = async() =>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments');
+    const data = await res.json();
+    console.log(data)
+}
+loadComments()
